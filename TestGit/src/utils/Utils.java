4e59d926313e10c028755a2c375fdee03313e629
Rederz <lurendel@gmail.com>
2016-08-03 13:33:32 -0600

    Prueba de cambio en branch de desarrollo

--- TestGit/src/utils/Utils.java
+++ TestGit/src/utils/Utils.java
@@ -1,5 +1,16 @@
 package utils;
 
+import java.util.List;
+
 public class Utils {
+
+	public static <T> boolean esListaVacia(List<T> lista) {
+
+		if (lista == null || lista.size() == 0) {
+			return true;
+		}
+
+		return false;
+	}
 
 }
